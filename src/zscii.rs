08198820a6316bebf43fe9004864e57e1f250714
Barry Corrigan <b.j.corrigan@gmail.com>
2023-06-28 20:59:10 +0100

    zscii

--- src/zscii.rs
+++ src/zscii.rs
@@ -1,3 +1,7 @@
+use std::string::FromUtf16Error;
+
+use crate::memory::Memory;
+
 // See 3.5.3 @ https://www.inform-fiction.org/zmachine/standards/z1point1/sect03.html
 const ZSCII_MAP234: [[char; 32]; 3] = [
     [
@@ -28,3 +32,29 @@ const ZSCII_MAP1: [[char; 32]; 3] = [
         '.', ',', '!', '?', '_', '#', '\'', '"', '/', '\\', '<', '-', ':', '(', ')',
     ],
 ];
+
+pub struct Zscii<'a> {
+    ptr: u16,
+    mode: u8,
+    buf: Vec<char>,
+    mem: &'a Memory,
+}
+
+impl<'a> Zscii<'a> {
+    pub fn new(mem: &Memory) -> Zscii {
+        Zscii {
+            ptr: 0u16,
+            mode: 0u8,
+            buf: vec![],
+            mem: mem,
+        }
+    }
+
+    pub fn get_string(&mut self, ptr: u16) -> String {
+        self.ptr = ptr;
+        self.mode = 0u8;
+        self.buf.clear();
+
+        "".to_owned()
+    }
+}
